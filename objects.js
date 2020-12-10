f2108c5eb656c72a7b21e6f2aa8e5a86aef9247d
Zoey Farago <zf.zoodle@gmail.com>
2020-12-10 05:33:56 +0000

    javascripting intro to objects

---
+++ objects.js
@@ -0,0 +1,12 @@
+const pizza = {  
+  toppings: ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+};
+
+const food = {
+  types: 'only pizza',
+};
+
+//console.log(pizza);
+console.log(food.types);
